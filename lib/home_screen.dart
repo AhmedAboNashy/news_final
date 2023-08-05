65b142abf2e8f8316eeb42944b6f13b9f0408983
AhmedAbonashy <ahmedabonashy1000@gmail.com>
2023-08-05 18:42:03 +0300

    create Project but empty

---
+++ lib/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeSceen extends StatelessWidget {
+static const String routeName='home';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:news/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+routes: {HomeSceen.routeName: (_)=>HomeSceen()},
+      initialRoute: HomeSceen.routeName,
+    );
+  }
+}
